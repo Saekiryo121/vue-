ce8ea6c56109555b18e878da3d725169fd5176e4
saeki <0ec6ky47385587v@gmail.com>
2023-10-30 16:31:07 +0900

    テスト

---
+++ app.js
@@ -0,0 +1,30 @@
+Vue.createApp({
+  data: function () {
+    return {
+      totalBaito: 15,
+        daysPerWeek: 5,
+        shiftSize: 6,
+        shiftSchedule: {},
+        shiftGenerated: false
+    }
+  },
+  methods: {
+    generateShift() {
+      this.shiftSchedule = {};
+      for (let day = 1; day <= this.daysPerWeek; day++) {
+        const shuffledBaito = this.shuffleBaito();
+        this.shiftSchedule[day] = shuffledBaito.slice(0, this.shiftSize);
+      }
+      this.shiftGenerated = true;
+    },
+    shuffleBaito() {
+      const baitoList = Array.from({ length: this.totalBaito }, (v, i) => `Baito ${i + 1}`);
+      for (let i = baitoList.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [baitoList[i], baitoList[j]] = [baitoList[j], baitoList[i]];
+      }
+      return baitoList;
+    }
+  }
+
+}).mount("#app");
